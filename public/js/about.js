5ad3e94821e2f631fd319a295eb3e9a18e10b75d
SunkenPotato <3.14radius02@gmail.com>
2024-06-13 12:34:38 +0200

    Update config.toml

--- public/js/about.js
+++ public/js/about.js
@@ -1,6 +1,11 @@
-function submitForm() {
-    var subj = $("contact-about").val()
-    var content = $("contact-text").val()
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    
+    var subject = document.getElementById('subject').value;
+    var content = document.getElementById('field0').value;
+    
+    var link = "mailto:sunkencouch67@gmail.com?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(content)
 
-    var link = 
-}
+    window.location.href = link;
+    
+});
