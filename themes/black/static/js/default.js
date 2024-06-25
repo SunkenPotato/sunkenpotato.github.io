05d2c412dfe192ba5e82a6881b73f3018b6b1d6b
SunkenPotato <3.14radius02@gmail.com>
2024-06-25 19:58:09 +0200

    Fixed fun feature #1.

--- themes/black/static/js/default.js
+++ themes/black/static/js/default.js
@@ -1,3 +1,23 @@
-const randomColor = "#"+((1<<24)*Math.random()|0).toString(16); 
+function getRandomColor() {
+    let color;
+    do {
+        color = Math.floor(Math.random() * 16777215).toString(16);
+        while (color.length < 6) {
+            color = "0" + color;
+        }
+        color = "#" + color;
+    } while (isTooDark(color));
+    return color;
+}
 
+function isTooDark(color) {
+    const r = parseInt(color.slice(1, 3), 16);
+    const g = parseInt(color.slice(3, 5), 16);
+    const b = parseInt(color.slice(5, 7), 16);
+
+    const brightness = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+    return brightness < 0.4; 
+}
+
+const randomColor = getRandomColor();
 document.documentElement.style.setProperty('--animate-color', randomColor);
